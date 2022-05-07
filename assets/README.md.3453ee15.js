763c7ce27ed790be1440799ba6c0cbf6bb24fb27
jun <493892452@qq.com>
2022-05-07 17:09:01 +0800

    修改

---
+++ assets/README.md.3453ee15.js
@@ -0,0 +1,9 @@
+import{_ as n,c as a,o as e,a as s}from"./app.1192f119.js";const m='{"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","slug":"\u5982\u4F55\u672C\u5730\u5F00\u53D1"}],"relativePath":"README.md"}',t={},o=s(`<h2 id="\u5982\u4F55\u672C\u5730\u5F00\u53D1" tabindex="-1">\u5982\u4F55\u672C\u5730\u5F00\u53D1 <a class="header-anchor" href="#\u5982\u4F55\u672C\u5730\u5F00\u53D1" aria-hidden="true">#</a></h2><div class="language-bash"><pre><code><span class="token comment"># \u514B\u9686\u672C\u4ED3\u5E93</span>
+$ <span class="token function">git</span> clone 
+
+<span class="token comment"># \u6216\u8005\u4F7F\u7528 yarn</span>
+$ <span class="token function">yarn</span> <span class="token function">install</span>
+
+<span class="token comment"># \u542F\u52A8\u5F00\u53D1\u670D\u52A1\u5668</span>
+$ <span class="token function">yarn</span> dev
+</code></pre></div>`,2),c=[o];function r(_,p,i,l,d,f){return e(),a("div",null,c)}var u=n(t,[["render",r]]);export{m as __pageData,u as default};
